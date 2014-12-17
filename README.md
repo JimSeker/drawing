e549fdb7c2cf6e7e38c9ab38d037ae62aff53ef9
JimSeker <seker@uwyo.edu>
2014-12-17 15:39:18 -0700

    prep to change to android studio.

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 Android Drawing Examples
 ===========
+eclipse/ has the eclipse projects, no longer updated.  Everything else is in andriod studio
 
 DrawDemo uses a canvas to draw on, then display via an imageview.
+
 DrawDemo2 uses a customview to draw on
+
 svtest is a very simple SurfaceView to draw and "animate" a moving box.
+
 GameDemo is a very simple space invaders like game using a surfaceview.
 
 These are example code for University of Wyoming, Cosc 4730 Mobile Programming course.
---
+++ eclipse/drawDemo/src/edu/cs4730/drawDemo/MainActivity.java
@@ -0,0 +1,39 @@
+package edu.cs4730.drawDemo;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new Draw_Fragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		//getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
