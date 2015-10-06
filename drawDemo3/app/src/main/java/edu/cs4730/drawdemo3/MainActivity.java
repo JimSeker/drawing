dd2e6e11285a36b4220f5a545683c61146dab7f2
JimSeker <seker@uwyo.edu>
2015-10-06 09:24:59 -0600

    finished drawdemo3 and moved svtest to legacy

--- drawDemo3/app/src/main/java/edu/cs4730/drawdemo3/MainActivity.java
+++ drawDemo3/app/src/main/java/edu/cs4730/drawdemo3/MainActivity.java
@@ -2,6 +2,9 @@ package edu.cs4730.drawdemo3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 
 /*
@@ -14,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new mySurfaceView(this));
+
     }
 }
--- drawDemo3/app/src/main/java/edu/cs4730/drawdemo3/mySurfaceView.java
+++ drawDemo3/app/src/main/java/edu/cs4730/drawdemo3/mySurfaceView.java
@@ -6,31 +6,52 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
+
+import java.util.Random;
 
 /**
+ *  In this example, we want movement even when the user is not interacting with the
+ *  screen.
+ *    An alien is randomly placed on the screen and the moves to the right.  It reset to the left
+ *    side if it is not touched.
+ *    if touched, it moves to another place on the screen.
+ *  The alien and movement is scaled based on the density of the screen as well.
  *
  */
 public class mySurfaceView extends SurfaceView implements SurfaceHolder.Callback{
 
     private myThread thread;
     public Paint  black;
-    public float x, y;
+    public int x, y;
+    private int height = 480, width=480 ;  //defaults incase not set yet.
+    private int alienheight, alienwidth;
+    public Rect myRect;
     Bitmap alien;
+    private Random myRand = new Random();
+    float scale;
 
     public mySurfaceView(Context context) {
         super(context);
 
         //load a picture and draw it onto the screen.
         alien = BitmapFactory.decodeResource(getResources(), R.drawable.alien);
+        scale = getResources().getDisplayMetrics().density; //this gives me the scale value for a mdpi baseline of 1.
+        //scale up the alien, so it bigger for dp
+        alienheight = (int) (alien.getHeight() * scale);
+        alienwidth = (int) (alien.getWidth() * scale);
+        myRect = new Rect();
+        x = myRand.nextInt(width - alienwidth);  //kept it inside the screen.
+        y = myRand.nextInt(height - alienheight);
+        myRect.set(x,y,x +alienwidth,y+alienheight);
 
-        black = new Paint();  //default is black
-        black.setStyle(Paint.Style.STROKE);
+        black = new Paint();  //default is black and we really are not using it.  need it to draw the alien.
 
+        //setup the thread for animation.
         getHolder().addCallback(this);
         thread = new myThread(getHolder(), this);
     }
@@ -38,14 +59,38 @@ public class mySurfaceView extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public void onDraw(Canvas c) {
         c.drawColor(Color.WHITE);
-        c.drawRect(x, y, x + 90, y + 90, black);
-        c.drawCircle(55, 55, 20, black);
+        c.drawBitmap(alien, null, myRect, black);
+
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        //everything is setup, now start.
+        height = getHeight();
+        width = getWidth();
         thread.setRunning(true);
         thread.start();
+    }
+    /*
+     * touch event to deal with when the user touches the alien.
+     *
+     */
+    @Override public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        // Retrieve the new x and y touch positions
+        int touchx = (int) event.getX();
+        int touchy = (int) event.getY();
+        if (myRect.contains(touchx,touchy)) {
+            //touched the alien
+            Log.v("hi", "collison");
+            x = myRand.nextInt(width - alienwidth);
+            y = myRand.nextInt(height - alienheight);
+            //we could set each manually, or just use the set method.
+           //myRect.left =  x;
+           // myRect.top = y;
+            myRect.set(x,y,x +alienwidth,y+alienheight);
+        }
+        return true;
     }
 
     @Override
@@ -101,8 +146,11 @@ public class mySurfaceView extends SurfaceView implements SurfaceHolder.Callback
                         _surfaceHolder.unlockCanvasAndPost(c);
                     }
                 }
-                x++;
-                if (x>90) {x=10;}
+                //move the alien accross the screen.
+                x += 2*scale;
+                if (x>width - alienwidth) {x=10;}
+                myRect.left = x; myRect.right =x + alienwidth;
+                //sleep for a short period of time.
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
