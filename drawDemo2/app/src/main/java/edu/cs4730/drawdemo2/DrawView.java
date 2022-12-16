a4f8967becef9d88764e9538bb356dd5fb0882e3
Jim Ward <seker@uwyo.edu>
2022-12-16 11:32:39 -0700

    simplified the example to be easier to understand.

--- drawDemo2/app/src/main/java/edu/cs4730/drawdemo2/DrawView.java
+++ drawDemo2/app/src/main/java/edu/cs4730/drawdemo2/DrawView.java
@@ -1,9 +1,6 @@
 package edu.cs4730.drawdemo2;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -13,17 +10,19 @@ import android.view.MotionEvent;
 import android.view.View;
 
 /**
- * This is a demo of a customview.  I'm ignoring some things like
+ * This is a demo of a customview. it draws 5 circles, which the user
+ * can click on to recolor.  I'm ignoring some things like
  * all the xml attributes that could be set.
  */
 public class DrawView extends View {
 
+
     Paint black, other;
-    public int incr, size = 8;
+    public int  size = 5;
     int mheight = 0, mwidth = 0;
-    int leftside, rightside, boardwidth;
-    public int[][] maze;
     public Context myContext;
+
+    circle[] myCircles = new circle[5];
 
     /*
      * default constructor
@@ -65,32 +64,36 @@ public class DrawView extends View {
         other = new Paint();  //I'll set  this one as I needed it.
         other.setStyle(Paint.Style.FILL);
 
-        if (maze != null) {
-            maze = null;
-        }
-        maze = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                maze[i][j] = Color.BLACK;
-            }
-        }
+        if (mwidth == 0) return;  //something isn't setup yet.
 
-        if (mheight > 0) setsizes();  //in case not on screen.
+        float radius = mwidth /5f;
+        //top left.
+        myCircles[0] = new circle(radius, radius,radius);
+
+        //top right
+        myCircles[1] = new circle(mwidth - radius,radius, radius);
+        //middle
+        myCircles[2] = new circle();
+        myCircles[2].r = radius;
+        myCircles[2].x = mwidth/2f;
+        myCircles[2].y = mheight/2f;
+
+        //top left.\
+        myCircles[3] = new circle();
+        myCircles[3].r = radius;
+        myCircles[3].x = radius;
+        myCircles[3].y = mheight - radius;
+        //top right
+        myCircles[4] = new circle();
+        myCircles[4].r = radius;
+        myCircles[4].x = mwidth - radius;
+        myCircles[4].y = mheight - radius;
+
+
+
     }
 
-    /*
-     * Setups up the default sizes for whatever the size of the screen is
-     * so that the grid takes up most of the space with a margin around it.
-     *
-     */
-    public void setsizes() {
 
-        incr = mwidth / (size + 2);  //give a margin.
-        leftside = incr - 1;
-        rightside = incr * 9;
-        boardwidth = incr * size;
-        Log.i("setsizes", "incr is " + incr);
-    }
 
     /*
      * clears the grid and then has the view redraw.
@@ -98,14 +101,12 @@ public class DrawView extends View {
 
     void clearmaze() {
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                maze[i][j] = Color.BLACK;
-            }
+           myCircles[i].color = Color.BLACK;
         }
         invalidate();
     }
 
-    /*
+    /**
      * (non-Javadoc)
      * @see android.view.View#onDraw(android.graphics.Canvas)
      *
@@ -118,76 +119,31 @@ public class DrawView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        int x = incr;
-        int y = incr;
-
         canvas.drawColor(Color.WHITE);
-        //top line  not needed anymore.
-        //canvas.drawLine(x, y -1, rightside, y-1, black);
-        //draw squares across, then down
-        for (int yi = 0; yi < size; yi++) {
-            for (int xi = 0; xi < size; xi++) {
-
-                canvas.drawRect(x, y, x + incr, y + incr, black);  //draw black box.
-                if (maze[xi][yi] != Color.BLACK) {
-                    Log.i("onDraw", "Drawing different color");
-                    other.setColor(maze[xi][yi]);
-                    canvas.drawRect(x + 1, y + 1, x + incr, y + incr, other);
-                }
-
-                x += incr; //move to next square across
-            }
-            x = incr;
-            y += incr;
+        if (mwidth ==0) return;
+        for (int i = 0; i < size; i++) {
+           other.setColor(myCircles[i].color);
+           canvas.drawCircle(myCircles[i].x, myCircles[i].y,myCircles[i].r, other);
         }
-        //bottom line  not needed anymore.
-        //canvas.drawLine(leftside, rightside, rightside, rightside,black);
+
     }
 
-    /*
-     * used by the ontouch event to figure out which box (if any) was "touched"
-     *
-     */
-    boolean where(int x, int y, int color, boolean recolor) {
-        int cx = -1, cy = -1;
-        if (y >= leftside && y < rightside &&
-            x >= leftside && x < rightside) {
-            y -= incr;
-            x -= incr; //simplifies the math here.
-            cx = x / incr;
-            cy = y / incr;
-            if (cx < size && cy < size) {
-                if (maze[cx][cy] == Color.BLACK) {
-                    maze[cx][cy] = color;
-                } else if (recolor)
-                    maze[cx][cy] = color;
 
-            } else {
-                Log.i("where", "Error in Where, cx=" + cx + " cy=" + cy);
+    boolean where(int x, int y, int color) {
+        float deltaX, deltaY;
+        double distance;
+        boolean collided;
+
+        for (int i = 0; i < size; i++) {
+            deltaX = myCircles[i].x - x;
+            deltaY = myCircles[i].y - y;
+            distance = Math.sqrt((deltaX*deltaX + deltaY*deltaY));
+            collided = (distance <= myCircles[i].r);
+            if (collided) {
+                myCircles[i].color = color;
+                return true;
             }
-            return true;
         }
-
-        /*
-         * If outside the lines, then popup a dialog and ask about reseting the board.
-         * Interestingly, nothing is deprecated in the view
-         */
-        Dialog dialog = null;
-        AlertDialog.Builder builder;
-        builder = new AlertDialog.Builder(myContext);
-        builder.setMessage("Reset board?")
-            .setCancelable(false)
-            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    clearmaze();
-                }
-            }).setNegativeButton("No", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                dialog.cancel();
-            }
-        });
-        dialog = builder.create();
-        dialog.show();
         return false;
     }
 
@@ -208,15 +164,11 @@ public class DrawView extends View {
         switch (action) {
             case MotionEvent.ACTION_DOWN:
                 Log.i("onTouchEvent", "Action_down");
-                where(x, y, Color.BLUE, true);
+                where(x, y, Color.BLUE);
                 invalidate();
                 break;
-            //       case MotionEvent. .CLICK:  //down click.  color RED
-            //           where(x,y,Color.RED, true);
-            //  return super.touchEvent(message);
             case MotionEvent.ACTION_MOVE:  //move not sure what to do yet.
-
-                where(x, y, Color.YELLOW, false);
+                where(x, y, Color.YELLOW);
                 invalidate();
                 break;
             case MotionEvent.ACTION_UP:
@@ -250,11 +202,27 @@ public class DrawView extends View {
         } else if (mheight > mwidth) {
             mheight = mwidth;  //make it square.
         }
-        setsizes();
         Log.i("MSW", "" + mwidth);
         Log.i("MSH", "" + mheight);
         //setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
         setMeasuredDimension(mwidth, mheight);
+        setup();
+    }
+
+     class circle {
+        float x;
+        float y;
+        float r;
+        int color;
+
+         public circle(float mx, float my, float mr) {
+             x=mx; y=my; r=mr; color=Color.BLACK;
+         }
+
+         public circle() {
+             x=0; y=0; r=0; color= Color.BLACK;
+         }
 
     }
+
 }
--- drawDemo2_tk/app/src/main/java/edu/cs4730/drawdemo2_tk/DrawView.kt
+++ drawDemo2_tk/app/src/main/java/edu/cs4730/drawdemo2_tk/DrawView.kt
@@ -1,7 +1,5 @@
 package edu.cs4730.drawdemo2_tk
 
-import android.app.AlertDialog
-import android.app.Dialog
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -11,42 +9,40 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 
+
 /**
- * This is a demo of a customview.  I'm ignoring some things like
+ * This is a demo of a customview. it draws 5 circles, which the user
+ * can click on to recolor.  I'm ignoring some things like
  * all the xml attributes that could be set.
  */
 class DrawView : View {
-    var black: Paint? = null
-    var other: Paint? = null
-    var incr = 0
-    var size = 8
+    lateinit var black: Paint
+    lateinit var other: Paint
+    var size = 5
     var mheight = 0
     var mwidth = 0
-    var leftside = 0
-    var rightside = 0
-    var boardwidth = 0
-    var maze: Array<IntArray>? = null
     var myContext: Context
+    var myCircles = arrayOfNulls<circle>(5)
 
     /*
-	 * default constructor
-	 */
+     * default constructor
+     */
     constructor(context: Context) : super(context) {
         myContext = context
         setup()
     }
 
     /*
-	 * should be the constructor that I'm calling!
-	 */
+     * should be the constructor that I'm calling!
+     */
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
         myContext = context
         setup()
     }
 
     /*
-	 * no clue on this constructor, it came with the example.
-	 */
+     * no clue on this constructor, it came with the example.
+     */
     constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(
         context,
         attrs,
@@ -57,143 +53,91 @@ class DrawView : View {
     }
 
     /*
-	 * Setups all the default variables.  Since I'm not sure which of the constructors is
-	 * called, I'm using this method and have all of them call it.
-	 */
+     * Setups all the default variables.  Since I'm not sure which of the constructors is
+     * called, I'm using this method and have all of them call it.
+     */
     fun setup() {
         black = Paint()
-        black!!.color = Color.BLACK //defaults to black, but just making sure.
-        black!!.style = Paint.Style.STROKE
+        black.color = Color.BLACK //defaults to black, but just making sure.
+        black.style = Paint.Style.STROKE
         other = Paint() //I'll set  this one as I needed it.
-        other!!.style = Paint.Style.FILL
-        if (maze != null) {
-            maze = null
-        }
-        maze = Array(size) { IntArray(size) }
-        for (i in 0 until size) {
-            for (j in 0 until size) {
-                maze!![i][j] = Color.BLACK
-            }
-        }
-        if (mheight > 0) setsizes() //in case not on screen.
+        other.style = Paint.Style.FILL
+        if (mwidth == 0) return  //something isn't setup yet.
+        val radius = mwidth / 5f
+        //top left.
+        myCircles[0] = circle(radius, radius, radius)
+
+        //top right
+        myCircles[1] = circle(mwidth - radius, radius, radius)
+        //middle
+        myCircles[2] = circle()
+        myCircles[2]!!.r = radius
+        myCircles[2]!!.x = mwidth / 2f
+        myCircles[2]!!.y = mheight / 2f
+
+        //top left.\
+        myCircles[3] = circle()
+        myCircles[3]!!.r = radius
+        myCircles[3]!!.x = radius
+        myCircles[3]!!.y = mheight - radius
+        //top right
+        myCircles[4] = circle()
+        myCircles[4]!!.r = radius
+        myCircles[4]!!.x = mwidth - radius
+        myCircles[4]!!.y = mheight - radius
     }
 
     /*
-	 * Setups up the default sizes for whatever the size of the screen is
-	 * so that the grid takes up most of the space with a margin around it.
-	 * 
-	 */
-    fun setsizes() {
-        incr = mwidth / (size + 2) //give a margin.
-        leftside = incr - 1
-        rightside = incr * 9
-        boardwidth = incr * size
-        Log.i("setsizes", "incr is $incr")
-    }
-
-    /*
-	 * clears the grid and then has the view redraw.
-	 */
+     * clears the grid and then has the view redraw.
+     */
     fun clearmaze() {
         for (i in 0 until size) {
-            for (j in 0 until size) {
-                maze!![i][j] = Color.BLACK
-            }
+            myCircles[i]!!.color = Color.BLACK
         }
         invalidate()
     }
 
-    /*
-	 * (non-Javadoc)
-	 * @see android.view.View#onDraw(android.graphics.Canvas)
-	 *
-	 * This is one of two methods I overrode.   The onDraw method
-	 * is the one that will draw everything for this view.  
-	 * in this case, an 8x8 grid on the screen.
-	 * 	 
-	 */
+    /**
+     * (non-Javadoc)
+     * @see android.view.View.onDraw
+     */
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        var x = incr
-        var y = incr
         canvas.drawColor(Color.WHITE)
-        //top line  not needed anymore.
-        //canvas.drawLine(x, y -1, rightside, y-1, black);
-        //draw squares across, then down
-        for (yi in 0 until size) {
-            for (xi in 0 until size) {
-                canvas.drawRect(
-                    x.toFloat(),
-                    y.toFloat(),
-                    (x + incr).toFloat(),
-                    (y + incr).toFloat(),
-                    black!!
-                ) //draw black box.
-                if (maze!![xi][yi] != Color.BLACK) {
-                    Log.i("onDraw", "Drawing different color")
-                    other!!.color = maze!![xi][yi]
-                    canvas.drawRect(
-                        (x + 1).toFloat(),
-                        (y + 1).toFloat(),
-                        (x + incr).toFloat(),
-                        (y + incr).toFloat(),
-                        other!!
-                    )
-                }
-                x += incr //move to next square across
-            }
-            x = incr
-            y += incr
+        if (mwidth == 0) return
+        for (i in 0 until size) {
+            other.color = myCircles[i]!!.color
+            canvas.drawCircle(
+                myCircles[i]!!.x, myCircles[i]!!.y, myCircles[i]!!.r,
+                other
+            )
         }
-        //bottom line  not needed anymore.
-        //canvas.drawLine(leftside, rightside, rightside, rightside,black);
     }
 
-    /*
-	 * used by the ontouch event to figure out which box (if any) was "touched"
-	 * 
-	 */
-    fun where(x: Int, y: Int, color: Int, recolor: Boolean): Boolean {
-        var x = x  //don't actually need another variable, but kotlin is dumb and won't let me change a parameter.
-        var y = y
-        var cx = -1
-        var cy = -1
-        if (y >= leftside && y < rightside && x >= leftside && x < rightside) {
-            y -= incr
-            x -= incr //simplifies the math here.
-            cx = x / incr
-            cy = y / incr
-            if (cx < size && cy < size) {
-                if (maze!![cx][cy] == Color.BLACK) {
-                    maze!![cx][cy] = color
-                } else if (recolor) maze!![cx][cy] = color
-            } else {
-                Log.i("where", "Error in Where, cx=$cx cy=$cy")
+    fun where(x: Int, y: Int, color: Int): Boolean {
+        var deltaX: Float
+        var deltaY: Float
+        var distance: Double
+        var collided: Boolean
+        for (i in 0 until size) {
+            deltaX = myCircles[i]!!.x - x
+            deltaY = myCircles[i]!!.y - y
+            distance = Math.sqrt((deltaX * deltaX + deltaY * deltaY).toDouble())
+            collided = distance <= myCircles[i]!!.r
+            if (collided) {
+                myCircles[i]!!.color = color
+                return true
             }
-            return true
         }
-
-        /* 
-         * If outside the lines, then popup a dialog and ask about reseting the board.
-         * Interestingly, nothing is deprecated in the view
-         */
-        var dialog: Dialog
-        val builder: AlertDialog.Builder = AlertDialog.Builder(myContext)
-        builder.setMessage("Reset board?")
-            .setCancelable(false)
-            .setPositiveButton("Yes") { dialog, id -> clearmaze() }
-            .setNegativeButton("No") { dialog, id -> dialog.cancel() }
-        dialog = builder.create()
-        dialog.show()
         return false
     }
 
     /*
      * (non-Javadoc)
      * @see android.view.View#onTouchEvent(android.view.MotionEvent)
-     * 
+     *
      * overrode this event to get all the touch events for this view.
-     * 
+     *
      */
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val action = event.action
@@ -203,27 +147,25 @@ class DrawView : View {
         when (action) {
             MotionEvent.ACTION_DOWN -> {
                 Log.i("onTouchEvent", "Action_down")
-                where(x, y, Color.BLUE, true)
+                where(x, y, Color.BLUE)
                 invalidate()
             }
             MotionEvent.ACTION_MOVE -> {
-                where(x, y, Color.YELLOW, false)
+                where(x, y, Color.YELLOW)
                 invalidate()
             }
-            MotionEvent.ACTION_UP -> {
-                performClick()
-            }
+            MotionEvent.ACTION_UP -> performClick()
         }
         return true
     }
 
     /**
-	 * using this to get the size of the view.
-	 * now... I should likely set the height if the
-	 * view is using wrapcontent instead matchparent
-	 * ie likely it comes back with a zero height or width!
-	 * and then nothing will be draw (no call to onDraw either).
-	 */
+     * using this to get the size of the view.
+     * now... I should likely set the height if the
+     * view is using wrapcontent instead matchparent
+     * ie likely it comes back with a zero height or width!
+     * and then nothing will be draw (no call to onDraw either).
+     */
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         Log.i("MSW", "" + measuredWidth)
@@ -231,15 +173,37 @@ class DrawView : View {
         mwidth = measuredWidth
         mheight = measuredHeight
         if (mheight > 0 && mwidth > mheight) {
-            mwidth = mheight  //make it square
+            mwidth = mheight //make it square.
         } else if (mheight == 0) {
             mheight = mwidth
         } else if (mheight > mwidth) {
-            mheight = mwidth //make it square
+            mheight = mwidth //make it square.
         }
-        setsizes()
-
+        Log.i("MSW", "" + mwidth)
+        Log.i("MSH", "" + mheight)
         //setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
         setMeasuredDimension(mwidth, mheight)
+        setup()
+    }
+
+    inner class circle {
+        var x: Float
+        var y: Float
+        var r: Float
+        var color: Int
+
+        constructor(mx: Float, my: Float, mr: Float) {
+            x = mx
+            y = my
+            r = mr
+            color = Color.BLACK
+        }
+
+        constructor() {
+            x = 0f
+            y = 0f
+            r = 0f
+            color = Color.BLACK
+        }
     }
 }
